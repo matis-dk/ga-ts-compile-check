d42c99db396e4e3c30386a522f3de4446e47c9cf
Mathias Piletti <mathiasp50@gmail.com>
2021-01-24 14:22:20 +0100

    p40

--- ts-compile-check/check.ts
+++ ts-compile-check/check.ts
@@ -1,15 +1,24 @@
 import * as glob from "glob";
 import * as core from "@actions/core";
 
-const spawnSync = require("child_process").spawnSync;
+import * as child from "child_process";
 
-async function start() {
+const { spawnSync } = child;
+
+type CompileError = {
+  projectPath: string;
+  stdout: string | null;
+  stderr: string | null;
+};
+
+(async function start() {
   const projects = await getProjects();
-  await runTypescriptCheck(projects);
-}
+  const compileErrors = runTypescriptCheck(projects);
+  logErrors(compileErrors);
+})();
 
 function getProjects(): Promise<string[]> {
-  return new Promise((res, rej) => {
+  return new Promise((res, _) => {
     glob("../**/tsconfig.json", {}, function (err, files) {
       if (err) {
         throw new Error("Failed search for tsconfig.json files");
@@ -30,16 +39,16 @@ function getProjects(): Promise<string[]> {
   });
 }
 
-async function runTypescriptCheck(projectPaths: string[]) {
+function runTypescriptCheck(projectPaths: string[]) {
   try {
-    const compileErrors = [];
+    const compileErrors: CompileError[] = [];
 
-    projectPaths.forEach((path) => {
+    projectPaths.forEach((projectPath) => {
       console.log("------------------------");
-      console.log(`Compile checking '${path}'`);
+      console.log(`Compile checking '${projectPath}'`);
 
       const spawnSyncOptions = {
-        cwd: path,
+        cwd: projectPath,
       };
 
       spawnSync("yarn", ["install"], spawnSyncOptions);
@@ -53,40 +62,39 @@ async function runTypescriptCheck(projectPaths: string[]) {
       const { status, stdout, stderr } = res;
       if (status !== 0) {
         compileErrors.push({
-          path,
-          response: {
-            status,
-            stdout: stdout?.toString(),
-            stderr: stderr?.toString(),
-          },
+          projectPath,
+          stdout: stdout?.toString(),
+          stderr: stderr?.toString(),
         });
       }
     });
 
-    if (compileErrors.length) {
-      console.log("------------------------");
-      core.setFailed(
-        `ERROR: Failed to compile ${compileErrors.length} project${
-          compileErrors.length > 1 ? `s` : ""
-        }`
-      );
-
-      compileErrors.forEach((c) => {
-        console.log("------------------------");
-        if (c.response.stdout) {
-          console.log(`Project '${c.path}' failed to compile`);
-          core.setFailed(c.response.stdout);
-        }
-        if (c.response.stderr) {
-          core.setFailed(c.response.stderr);
-        }
-      });
-      process.exit();
-    }
+    return compileErrors;
   } catch (err) {
     core.setFailed("ERROR: Failed to run childProcess");
     throw new Error(err);
   }
 }
 
-start();
+function logErrors(compileErrors: CompileError[]) {
+  if (compileErrors.length) {
+    console.log("------------------------");
+    core.setFailed(
+      `ERROR: Failed to compile ${compileErrors.length} project${
+        compileErrors.length > 1 ? `s` : ""
+      }`
+    );
+
+    compileErrors.forEach((c) => {
+      console.log("------------------------");
+      if (c.stdout) {
+        core.info(`\u001b[Project '${c.projectPath}' failed to compile]`);
+        core.setFailed(c.stdout);
+      }
+      if (c.stderr) {
+        core.setFailed(c.stderr);
+      }
+    });
+    process.exit();
+  }
+}
